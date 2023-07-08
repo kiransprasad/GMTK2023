3f73cc941579b4f103a43e6fbfa5dc1f90c53bf5
KiranPrasadStPats <98070735+KiranPrasadStPats@users.noreply.github.com>
2023-07-08 02:59:43 -0400

    Final commit for Day 1

--- GMTKGameJam2023/Assets/Scripts/StreamOverlay.cs
+++ GMTKGameJam2023/Assets/Scripts/StreamOverlay.cs
@@ -2,22 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class StreamOverlay : MonoBehaviour
 {
-
-    public int speed;
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        // bewber
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.right * speed * Time.deltaTime;
+        
     }
-
-
 }
