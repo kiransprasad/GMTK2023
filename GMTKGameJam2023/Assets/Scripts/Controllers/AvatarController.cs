52a8fa614d3c82bf24c108540cbd0b376492f9a7
KiranPrasadStPats <98070735+KiranPrasadStPats@users.noreply.github.com>
2023-07-09 03:42:14 -0400

    Update AvatarController.cs

--- GMTKGameJam2023/Assets/Scripts/Controllers/AvatarController.cs
+++ GMTKGameJam2023/Assets/Scripts/Controllers/AvatarController.cs
@@ -8,20 +8,53 @@ public class AvatarController : MonoBehaviour
     [SerializeField]
     public PlayerController player;
 
+    BoxCollider2D collider;
+    const float height = 0.5f;
+
+    // X-movement
     float moveSpeed;
+
+    // Y-movement
+    bool grounded;
+    float yVelocity;
+
 
     // Start is called before the first frame update
     void Start()
     {
 
+        collider = GetComponent<BoxCollider2D>();
+
+        // X-movement
         moveSpeed = 4;
+
+        // Y-movement
+        grounded = true;
+        yVelocity = 0;
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // FixedUpdate updates with the Physics engine
+    // Ground check is in here
+    private void FixedUpdate() {
+
+        if(Input.GetKeyDown(KeyCode.J)) {
+            Jump();
+        }
+
+        // Raycast Down
+        RaycastHit2D ray = Physics2D.Raycast(collider.bounds.center, Vector2.down, collider.bounds.extents.y + 0.1f);
+
+        if(ray.collider != null) {
+            grounded = true;
+            yVelocity = 0;
+            transform.position = new Vector3(transform.position.x, ray.point.y + height, 0);
+        }
+        else {
+            yVelocity -= 0.1f;
+            transform.position += new Vector3(0, yVelocity, 0);
+        }
+    
     }
 
     // Returns a boolean as to whether or not the player reached their destination
@@ -52,8 +85,10 @@ public class AvatarController : MonoBehaviour
 
     public void Jump() {
 
-
-
+        if(grounded) {
+            grounded = false;
+            yVelocity = 1;
+        }
     }
 
     // MENTOR-SPECIFIC
