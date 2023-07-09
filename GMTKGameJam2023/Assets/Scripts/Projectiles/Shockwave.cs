97e082a907fce4ad352ab5be8f89e1a0d37cc38e
KiranPrasadStPats <98070735+KiranPrasadStPats@users.noreply.github.com>
2023-07-08 23:04:21 -0400

    Shockwave

--- GMTKGameJam2023/Assets/Scripts/Projectiles/Shockwave.cs
+++ GMTKGameJam2023/Assets/Scripts/Projectiles/Shockwave.cs
@@ -13,6 +13,7 @@ public class Shockwave : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+        if(transform.localScale.x > 23.5f) Destroy(gameObject);
     }
 }
